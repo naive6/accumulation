928be9e5dfccfa5a015ab97564148e91c083b474
naive6 <3066364558@qq.com>
2018-07-13 09:34:01 +0800

    测试excel生成方法

--- src/main/java/tests/Test.java
+++ src/main/java/tests/Test.java
@@ -1,7 +1,47 @@
 package tests;
 
-public class Test {
+public class Test implements Cloneable {
+    private int a;
     public static void main(String[] args) {
+        Test test=new Test();
+        test.setA(1);
+        Test test1=test.clone();
+        System.out.println(test1.getA());
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public Test clone()
+
+    {
+
+        Object obj = null;
+
+        try
+
+        {
+
+            obj = super.clone();
+
+            return (Test) obj;
+
+        }
+
+        catch(CloneNotSupportedException e)
+
+        {
+
+            System.out.println("不支持复制！");
+
+            return null;
+
+        }
 
     }
 }
